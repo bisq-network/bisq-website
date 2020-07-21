2d3b27a6f8e2c3a1e29bf084b51e26286c8b8e03
pedromvpg <pedro.g@pvxg.net>
2020-07-20 23:46:22 -0400

    Add coockie script and detection

--- js/scripts.js
+++ js/scripts.js
@@ -150,9 +150,9 @@ $( document ).ready( function() {
         return;
     }
 
-
     $( ".mode-toggle" ).on( "click", function() {
       $('body').toggleClass("dark-mode");
+      $('body').hasClass("dark-mode") ? Cookies.set('darkmode', 1) : Cookies.set('darkmode', 0);
       var text = $(".mode-toggle").text();
       $(".mode-toggle").text(text == "Light" ? "Dark" : "Light");
       return;

597340117a207e2318177fdd1f9d3ec4f2cfb0a3
m52go <mfiver@gmail.com>
2020-08-10 09:26:19 -0400

    Add new tag to highlight current page in nav

--- _plugins/custom_tag.rb
+++ _plugins/custom_tag.rb
@@ -14,3 +14,21 @@ class LingualLink < Liquid::Tag
 end
 
 Liquid::Template.register_tag('lingual_link', LingualLink)
+
+class IsCurrentPage < Liquid::Tag
+  def initialize(tag_name, dest, _tokens)
+    super
+    @dest = dest
+  end
+
+  def render(context)
+    urlParts = ( context.environments.first["page"]["url"] ).split('/')
+    urlParts.each_with_index do |part,index|
+      if( ( part.strip == @dest.strip ) && ( index == 2 || index == 1 ) )
+        return 'current'
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('is_current_page', IsCurrentPage)

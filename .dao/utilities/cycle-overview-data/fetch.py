0cd9cad68aed6afc8c147334ade0a2b9100d761c
m52go <735155+m52go@users.noreply.github.com>
2021-10-27 12:55:12 -0400

    Add block times to cycle overview data dump

--- .dao/utilities/cycle-overview-data/fetch.py
+++ .dao/utilities/cycle-overview-data/fetch.py
@@ -15,6 +15,22 @@ daoCycleBurnDataFilePath = 'data/dao-burn-cycle.json'
 settingsFilePath = 'settings.json'
 pathError = 'Error finding DAO voting results file...did you put the right path in ' + settingsFilePath + '? You must specify the full path.'
 
+### return cycle start and end blocks from cycle number
+
+def getCycleBlocks( cycle ):
+
+    cycleStart = 571747 - 1 # first block was 571747, but first cycle was 1 block off
+    cycleLength = 4680
+
+    i = 1
+    while( i < cycle ):
+        i += 1
+        cycleStart = cycleStart + cycleLength
+
+    cycleEnd = cycleStart + cycleLength
+
+    return { 'start': cycleStart+1, 'end': cycleEnd }
+
 ### get settings
 
 try:
@@ -37,22 +53,26 @@ except:
     sys.exit()
 
 daoBurnDataObj = json.loads(daoBurnData)
-    
+
 ### put all data together
 
 with open( daoResultsPath, 'r' ) as daoResults:
 
-        daoResultsDict = json.loads( daoResults.read() )
-        
-        # assume cycle that started more than 40 days ago has completed
-        for cycle in daoResultsDict:
-            if( ( timeNow - ( cycle['startTime'] / 1000 ) > 3456000 ) ):
-                cycleOverviewData[ str(cycle['cycleIndex']) ] = {}
-                cycleOverviewData[ str(cycle['cycleIndex']) ]['startTime'] = math.floor( cycle['startTime'] / 1000 )
-                cycleOverviewData[ str(cycle['cycleIndex']) ]['issuance'] = float( cycle['issuance'].split(" ")[0] )
-                cycleOverviewData[ str(cycle['cycleIndex']) ]['burn'] = daoBurnDataObj[ str(cycle['cycleIndex']) ]['TOTAL']['feeSum']
-                cycleOverviewData[ str(cycle['cycleIndex']) ]['numberProposals'] = cycle['numberOfProposals']
+    daoResultsDict = json.loads( daoResults.read() )
 
+    # assume cycle that started more than 40 days ago has completed
+    i = 0
+    for cycle in daoResultsDict:
+        i = i + 1
+        if( ( timeNow - ( cycle['startTime'] / 1000 ) > 3456000 ) ):
+            cycleOverviewData[ str(cycle['cycleIndex']) ] = {}
+            cycleOverviewData[ str(cycle['cycleIndex']) ]['startTime'] = math.floor( cycle['startTime'] / 1000 )
+            cycleOverviewData[ str(cycle['cycleIndex']) ]['endTime'] = math.floor( ( ( daoResultsDict[ i ]['startTime'] ) / 1000 ) ) - 1
+            cycleOverviewData[ str(cycle['cycleIndex']) ]['startBlock'] = getCycleBlocks(i)['start']
+            cycleOverviewData[ str(cycle['cycleIndex']) ]['endBlock'] = getCycleBlocks(i)['end']
+            cycleOverviewData[ str(cycle['cycleIndex']) ]['issuance'] = float( cycle['issuance'].split(" ")[0] )
+            cycleOverviewData[ str(cycle['cycleIndex']) ]['burn'] = daoBurnDataObj[ str(cycle['cycleIndex']) ]['TOTAL']['feeSum']
+            cycleOverviewData[ str(cycle['cycleIndex']) ]['numberProposals'] = cycle['numberOfProposals']
 
 with open( 'data/cycle-overview-data.json', 'w' ) as f:
     f.write( json.dumps( cycleOverviewData, indent=4 ) )
